89062f42dba8783c20bccc7a80f0ab29ba75b1ba
Renan Queiroz <renanq@gmail.com>
2020-03-25 19:57:05 -0300

    Layout frontend pronto

---
+++ frontend/src/pages/NewIncident/index.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import {FiArrowLeft} from 'react-icons/fi';
+
+import './styles.css';
+import logoImg from '../../assets/logo.svg';
+
+export default function NewIncident() {
+    return (
+        <div className="new-incident-container">
+            <div className="content">
+                <section>
+                    <img src={logoImg} alt="Be The Hero" />
+                    <h1>Cadastrar Novo Caso</h1>
+                    <p>Descreva o caso detalhadamente para encontrar um herói para resolver isso.</p>
+                    <Link className="back-link" to="/profile">
+                        <FiArrowLeft size={16} color="#E02041"/>Voltar para a home
+                    </Link>
+                </section>
+
+                <form>
+                    <input placeholder="Título do Caso" />
+                    <textarea placeholder="Descrição" />
+                    <input placeholder="Valor em reais" />
+                    <button classNae="button" type="submit"/>
+                </form>
+            </div>
+        </div>
+    )
+}
